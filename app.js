003f720bacd6031b60af4edd1653aa2b13e32a76
Muhammad Utsman <kucingapes@gmail.com>
2019-11-06 10:39:31 +0700

    add test emiter android

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ var io = require('socket.io').listen(app);
 
 // Send current time to all connected clients
 function sendTime() {
-    io.sockets.emit('time', { time: new Date().toJSON() });
+    io.sockets.emit('time', {
+        time: new Date().toJSON() 
+    });
 }
+
+io.sockets.on('android-test', function(socket) {
+    console.log('receiver from android')
+})
+
+io.sockets.on('user', function(data) {
+    console.log('reveceiver from ' + data.message)
+})
 
 // Send current time every 10 secs
 setInterval(sendTime, 10000);
@@ -27,6 +37,8 @@ io.sockets.on('connection', function(socket) {
     socket.on('i am client', function() {
       console.log('received a message from the client.');
     });
+
+    //document.getElementById("btn").onclick = socket.emit('user-test', {data: 'anjay'});
 });
 
 const PORT = process.env.PORT || 3000;
